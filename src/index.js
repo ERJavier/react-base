7fddd1c03b355f3db9207946286350f6b5b3f865
Javier Cepeda <erjavierc@gmail.com>
2022-10-24 08:43:12 -0400

    render

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './App/index.js';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
